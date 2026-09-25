9e4b0c2f41d9ca9c5e10241f5849ded2bf371164
Hannes <hannes.kontos@gmx.de>
2026-09-25 20:15:25 +0200

    Improve repo (#4)

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-username = ''
-password = ''
-server = ''
-useragent = 'webuntis-ical'
-school = ''
+username = ''  # Untis account username
+password = ''  # Untis account passord
+server = '' # Untis server
+useragent = 'webuntis-ical' # User-agent (not-important)
+school = '' # Untis school id
 
 timezone = 'Europe/Berlin'  # Timezone
 ics_location = '/home/user/Downloads/'  # Location where ics files will be stored
@@ -11,8 +11,10 @@ schoolYear = None  # yyyy/yyyy or None for current
 startDate = "22.09.2026" # In dd.mm.yy
 endDate = "28.09.2026" # In dd.mm.yy
 
+# Your klass
 klass = ""
 
+# List of subjects. If empty all subjects
 subjects = [
 
 ]
